2a6b43523c58d2375b26b62df802727eafe7c43b
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2026-06-17 13:13:00 +0200

    removed commentaires 2

--- projects/views.py
+++ projects/views.py
@@ -28,7 +28,7 @@ from drf_spectacular.utils import (
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all().order_by("name")
     serializer_class = CategorySerializer
-     permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
