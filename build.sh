4e3ecf50a44427790d9eb16717f511df9e05a011
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2026-06-17 14:27:11 +0200

    edited build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,27 @@ set -o errexit
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+echo "Applying database migrations..."
+python manage.py migrate
+
+echo "Creating production superuser..."
+python manage.py create_production_superuser
+
+echo "Build finished successfully!"set -o errexit
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
 
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
-
 echo "Applying database migrations..."
 python manage.py migrate
+
+echo "Creating production superuser..."
+python manage.py create_production_superuser
 
 echo "Build finished successfully!"
