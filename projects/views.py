b3c51a8d6edd04de1bee44a8f0b6d524a6f743bb
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2026-06-18 20:17:44 +0200

    add permission for readOnly

--- projects/views.py
+++ projects/views.py
@@ -42,7 +42,8 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == request.user
+        # Только автор или суперюзер
+        return obj.author == request.user or request.user.is_superuser
 
 
 @extend_schema_view(
