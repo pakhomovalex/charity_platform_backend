41e4a5f6283f601d981c19dff18d800f0c9f9ef4
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2026-06-20 00:21:59 +0200

    works add email send service 5

--- config/settings.py
+++ config/settings.py
@@ -316,16 +316,6 @@ JAZZMIN_UI_TWEAKS = {
 }
 
 # --- Email ---
-# if not DEBUG:
-#     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-#     EMAIL_HOST = "smtp.gmail.com"
-#     EMAIL_PORT = 587
-#     EMAIL_USE_TLS = True
-#     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-#     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-#     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-# else:
-#     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 EMAIL_BACKEND = "config.email_backends.SendGridBackend"
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "pakhomov.alex.v@gmail.com")
